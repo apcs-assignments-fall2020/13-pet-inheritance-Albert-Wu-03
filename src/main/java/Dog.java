45616cbc4c161bf4b2da7c1ed5234c16845b7a1e
Albert-Wu-03 <22qwu@athenian.org>
2020-10-29 03:38:09 +0000

    Albert

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,39 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog(String name, int age, String c){
+        super(name, age);
+        this.breed = c;
+    }
+
+    public Dog(){
+        super("Max", 1);
+        this.breed = "Dog";
+
+    }
 
     // makeNoise() method
-
+    public void makeNoise() {
+        System.out.println("Wang"); //I don't know how to type a dog's noise.
+    }
 
     // toString method
-
+    public String toString() {
+        String str = "Name: " + this.getName() + ", Age: " + this.getAge() + ", Breed: " + this.breed;
+        return str;
+    }
 
     // Getter
-
+    public String getBreed(){
+        return this.breed;
+    }
 
     // Setter
+    public void setBreed(String breed){
+         if(breed.trim().length() != 0){
+            this.breed = breed;
+         }
+    }
 }
